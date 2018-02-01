7a43614f3366afca20fea8cfc11cce73de31c0ba
Rolf Timmermans <rolftimmermans@voormedia.com>
2018-02-01 12:08:45 +0100

    Improve rethinkdb recipe.

--- rethinkdb/run.sh
+++ rethinkdb/run.sh
@@ -8,7 +8,7 @@ ca_path="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 svc_endpoint="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}/api/v1/namespaces/${POD_NAMESPACE}/endpoints/rethinkdb"
 
 # Get IPs of other pods in this stateful set, if any.
-existing=$(wget -q --ca-certificate "${ca_path}" --header "Authorization: Bearer ${token}" -O - "${svc_endpoint}" | jq -s -r --arg h "${POD_IP}" '.[0].subsets | .[].addresses // [] | [ .[].ip ] | map(select(. != $h)) | .[0]') || exit 1
+existing=$(wget -q --ca-certificate "${ca_path}" --header "Authorization: Bearer ${token}" -O - "${svc_endpoint}" | jq -s -r --arg h "${POD_IP}" '.[0].subsets | .[].addresses // [] | [ .[].ip ] | map(select(. != $h)) | .[0]' // "") || exit 1
 
 # If there are no other pods, check if we need to join ourselves or
 if [[ -n "${existing}" ]]; then
